9a9680ea63b6fe056619df44a6295d3286925fde
ChernyshYuriy <yuriy.chernysh97@gmail.com>
2020-02-06 18:09:23 +0200

    1.0

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".slider").slick({
+        slidesToShow: 3,
+        variableWidth: true
+    })
+});
+
+ new fullpage("#fullpage",{
+     autoScrolling: true,
+     navigation: true,
+     navigationPosition: 'left',
+  });
+ 
+
+ 
+ 
+ 
